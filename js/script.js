4092ae3e68f7f96574db65d2b2f19e34760b4cee
martinaapalazzi <martinaapalazzi@gmail.com>
2023-11-30 17:38:15 +0100

    Trial with card

--- js/script.js
+++ js/script.js
@@ -6,11 +6,11 @@ const cognome = prompt("Qual'è il tuo cognome?")
 const colore = prompt("Qual'è il tuo colore preferito?")
 
 
- const password = nome + cognome + colore + 114
+const password = nome + cognome + colore + 114
 
 console.log(password)
 
- document.writeln ( `${password}` )
+document.writeln ( `${password}` )
 
-const myElement = document.getElementById('target')
+const myElement = document.getElementById('card')
 myElement.innerHTML = password;
