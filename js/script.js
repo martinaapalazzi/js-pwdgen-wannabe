966b085cf0f054eb6bb71daddf122355d609d3dd
martinaapalazzi <martinaapalazzi@gmail.com>
2023-11-30 15:31:57 +0100

    add promts + lets

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let text = prompt {
+    "Come ti chiami?"
+}
+
+let stringUno = "Martina"
+
+let text = prompt {
+    "Qual'è il tuo cognome?"
+}
+
+let stringDue = "Palazzi"
+
+let text = prompt {
+    "Qual'è il tuo colore preferito?"
+}
+
+let stringTre = "Rosso"
